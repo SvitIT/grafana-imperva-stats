faf0debf430e03ab6081b4264aa2438b465397a3
Chmele <killerbbb123@gmail.com>
2023-03-16 17:45:57 +0200

    generalize IOZip class for local storage files

--- iozip.py
+++ iozip.py
@@ -1,7 +1,7 @@
+from io import StringIO, BytesIO
+from itertools import groupby, chain
+from csv import DictReader
 from zipfile import ZipFile
-import io
-import csv
-import itertools
 from datetime import datetime
 
 
@@ -11,47 +11,49 @@ class IOZip:
             self.zipfile = ZipFile(tempfile._file)
         except AttributeError:
             self.zipfile = ZipFile(tempfile)
-        self.plain = self.get_plain_data()
+
+    @property
+    def plain(self):
+        return self.get_plain_data()
 
     def get_first_file(self):
         return self.zipfile.read(self.zipfile.filelist[0])
 
     def get_plain_data(self):
-        pass
+        return [self.get_first_file()]
+
+    def make_payload(self, measurement):
+        yield from (substitute_point(measurement, time, raw)
+                    for row in self.plain 
+                    for time, raw in group_bytes_by_time(row))
+
+    def flat_payload(self, measurement):
+        return chain.from_iterable(self.make_payload(measurement))
 
 
 class ImpervaLog(IOZip):
     def get_plain_data(self):
         return [
-            IOZip(io.BytesIO(self.zipfile.read(i))).get_first_file() 
+            IOZip(BytesIO(self.zipfile.read(i))).get_first_file() 
             for i in self.zipfile.filelist]
 
-    def make_payload(self, measurement):
-        yield from (substitute(measurement, time, raw) for row in self.plain for time, raw in group_bytes_by_time(row))
 
-    def flat_payload(self, measurement):
-        return itertools.chain.from_iterable(self.make_payload(measurement))
-
-def substitute(measurement, time, raw):
-    fields = {i["IndicationName"]: i for i in raw}
-    yield from ({
+def substitute_point(measurement, time, raw):
+    yield {
         'measurement': measurement,
         'time': datetime.strptime(time, '%Y-%m-%d %H:%M:%S').isoformat()+'Z',
-        'fields': {
-                field+'Max': int(fields[field]['MaxValue']),
-                field+'Avg': int(fields[field]['AverageValue']),
-            }
-    }for field in fields)
+        'fields': substitute_fields(raw)
+    }
+
+def substitute_fields(raw):
+    data = {i["IndicationName"]: i for i in raw}
+    postfix_field = {'Max': 'MaxValue', 'Avg': 'AverageValue'}
+    return {row+postfix: int(data[row][field])for postfix, field in postfix_field.items() for row in data}
 
 def bytes_to_csv_dict(b):
-    return csv.DictReader(io.StringIO(b.decode()))
+    return DictReader(StringIO(b.decode()))
 
 def group_bytes_by_time(csv_dict):
-    return itertools.groupby(bytes_to_csv_dict(csv_dict), lambda x: x["TimeStamp"])
+    return groupby(bytes_to_csv_dict(csv_dict), lambda x: x["TimeStamp"])
 
-
-
-f = open("unpack/spam.zip", 'rb')
-i = ImpervaLog(f)
-i.get_plain_data()
-i.make_payload('gw1')
+# list(ImpervaLog(open("unpack/spam.zip", 'rb')).flat_payload('gw1'))
